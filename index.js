775d5ae3c3291ac644632d89d1093a6731f6910d
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2021-12-03 21:11:34 +0400

    Lessone №1
    Выполнил первый урок, но есть проблема: не выводится вторая переменная массива, даже там стоит не число и не простое число.

---
+++ index.js
@@ -0,0 +1,22 @@
+const colors=require('colors');
+
+const numb=process.argv.slice(2);
+
+for(let i=0;i<numb.length;i++){
+    if(isNaN(numb[i])){
+        console.log("we have a trator");
+        break;
+    }else if(numb[i]%2==0){
+        console.log(colors.red('WARNING!We need only simple numbers.'));
+        break;
+    } else {
+        if(i==0){
+            console.log(colors.red(numb[i]))
+            i++;
+        }else if(i==1){
+            console.log(colors.yellow(numb[i]))
+        }else if(i==2){
+            console.log(colors.green(numb[i]))
+        }  
+    }
+}
